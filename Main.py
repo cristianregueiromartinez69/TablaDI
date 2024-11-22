eb9fd6bbf5f4661631cb96fddbbdfafd9a4d018a
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-22 13:06:41 +0100

    haciendo vista

--- Main.py
+++ Main.py
@@ -1,26 +1,42 @@
 import sys
 
-from PyQt6.QtWidgets import (QMainWindow, QApplication, QVBoxLayout, QWidget,
-                             QListView, QPushButton)
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QWidget, QVBoxLayout, QTableView)
+
+from ModeloTabla import ModeloTabla
 
 
-class MainWindow(QMainWindow):
+class TableView(QMainWindow):
     def __init__(self):
         super().__init__()
 
         super().__init__()
-        self.setWindowTitle("TODO")
+        self.setWindowTitle("Ejemplo de table view")
         self.setFixedSize(400, 400)
 
-        
+        self.datos = [["Nombre", "Dni", "Genero", "Fallecido"],
+                      ["Ana perez", "123123123F", "Mujer", False],
+                      ["Paco Jémez", "67676767H", "Hombre", True],
+                      ["Victor Roque", "12389065H", "Hombre", False],
+                      ["Juanita Sainz", "23423423D", "Mujer", True]]
+
+        caja = QVBoxLayout()
+        self.tvwTabla = QTableView()
+        modelo = ModeloTabla(self.datos)
+        self.tvwTabla.setModel(modelo)
+
+        container = QWidget()
+
+        self.setCentralWidget(container)
+        self.show()
+
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = TableView()
     app.exec()
 
 
-#Todo: borrar, tenemos que detectar lo que este seleccionado para borrarlo
 
 
