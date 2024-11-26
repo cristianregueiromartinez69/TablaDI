c53b26cb6d513874d648f6dfd520fa0536de74ef
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-26 10:09:26 +0100

    metodo seleccion de tablas para introducir datos

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import (QMainWindow, QApplication, QWidget, QVBoxLayout, QTableView, QHBoxLayout, QLineEdit)
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QWidget, QVBoxLayout, QTableView, QHBoxLayout, QLineEdit,
+                             QComboBox, QCheckBox)
 
 from ModeloTabla import ModeloTabla
 
@@ -24,6 +25,7 @@ class TableView(QMainWindow):
 
         caja = QVBoxLayout()
         self.tvwTabla = QTableView()
+        self.tvwTabla.clicked.connect(self.on_cell_clicked)
         modelo = ModeloTabla(self.datos)
         self.tvwTabla.setModel(modelo)
 
@@ -31,19 +33,23 @@ class TableView(QMainWindow):
 
         self.cabecera = self.tvwTabla.horizontalHeader()
         cajaHorizontal = QHBoxLayout()
-        caja.addLayout(cajaHorizontal)
         self.txtNombre = QLineEdit()
         cajaHorizontal.addWidget(self.txtNombre)
 
         self.txtDni = QLineEdit()
         cajaHorizontal.addWidget(self.txtDni)
-        self.txtGenero = QLineEdit()
-        cajaHorizontal.addWidget(self.txtGenero)
-        self.txtFallecido = QLineEdit()
-        cajaHorizontal.addWidget(self.txtFallecido)
+        self.cmbGenero = QComboBox()
+        self.cmbGenero.addItems(["Mujer", "Hombre", "Otro"])
+
+        cajaHorizontal.addWidget(self.cmbGenero)
+
+        self.chkFallecido = QCheckBox("Fallecido")
+        cajaHorizontal.addWidget(self.chkFallecido)
+
 
 
         caja.addWidget(self.tvwTabla)
+        caja.addLayout(cajaHorizontal)
         container = QWidget()
         container.setLayout(caja)
         self.setCentralWidget(container)
@@ -52,10 +58,42 @@ class TableView(QMainWindow):
 
 
 
+    def checkGenero(self, genero):
+        if genero == "Mujer":
+            self.cmbGenero.setCurrentIndex(0)
+        elif genero == "Hombre":
+            self.cmbGenero.setCurrentIndex(1)
+        else:
+            self.cmbGenero.setCurrentIndex(2)
+
+    def checkFallecido(self, estado):
+        if estado:
+            self.chkFallecido.setChecked(True)
+        else:
+            self.chkFallecido.setChecked(False)
+
+
+    def on_cell_clicked(self, index):
+        fila = index.row()
+        datos_fila = self.datos[fila]
+
+        self.txtNombre.setText(datos_fila[0])
+        self.txtDni.setText(datos_fila[1])
+        self.checkGenero(datos_fila[2])
+        self.checkFallecido(datos_fila[3])
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = TableView()
     app.exec()
+
+
+
+'''
+Hacer que al pulsar en una fila, se manden los datos a los campos de abajo
+'''
 
 
 
